08acdfc7e412cd6f91770f6a1f061d0d766e6c52
Venugopal Mankani <venumankani@gmail.com>
2023-10-02 11:35:52 +0530

    draw dots | Undo | Redo buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState, type MouseEvent } from 'react';
 import './App.css';
+import { Button } from '@mui/material';
+
+interface Dots {
+  x: number,
+  y: number
+}
 
 function App() {
+
+  const [dots, setDots] = useState<Dots[]>([])
+  const [dotList, setDotList] = useState<Dots[]>([])
+
+  const undo = () => {
+    const lastDot = dots[dots.length - 1];
+    setDots(dots.slice(0, -1));
+    setDotList([...dotList, lastDot]);
+  }
+
+  const redo = () => {
+    const lastDot = dotList[dotList.length - 1];
+    if (lastDot) {
+      setDotList(dotList.slice(0, -1));
+      setDots([...dots, lastDot]);
+    }
+  }
+
+  const clickScreen = (e: MouseEvent) => {
+    const { clientX, clientY } = e;
+    setDots([...dots, { x: clientX, y: clientY }]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='buttons'>
+        <Button onClick={undo} variant='contained' color='secondary'>Undo</Button>
+        <Button onClick={redo} variant='contained' color='secondary'>Redo</Button>
+      </div>
+      <div className='clickArea' onClick={clickScreen} >
+        {dots.map(({ x, y }: Dots, id: number) => (
+          <div className='dot'
+            key={id}
+            style={{
+              left: x, top: y
+            }} />
+        ))}
+      </div>
     </div>
   );
 }
